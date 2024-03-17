0da86a9243d7929cd58effbfe7003521253a5324
Zachary Zak <zachzak@hotmail.com>
2024-03-17 17:29:14 -0500

    Fix dropdown status select

--- Lister.Web/wwwroot/js/todoitems.js
+++ Lister.Web/wwwroot/js/todoitems.js
@@ -15,12 +15,17 @@ function deleteToDo(id) {
     });
 }
 
-function updateState(id, state) {
+function updateState(id) {
+    var selectList = document.getElementById('select_' + id);
+    
+    var newState = selectList.value;
+    
+
     $.ajax({
         type: "POST",
-        url: window.location + '?handler=UpdateStatus', // <-- Where should this point?
+        url: window.location + '?handler=UpdateStatus',
         contenttype: 'application/x-www-form-urlencoded; charset=UTF-8',
-        data: { id: id, state: state},
+        data: { id: id, state: newState},
         headers: {
             RequestVerificationToken: token
         },
